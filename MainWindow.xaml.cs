96ac7ec9c3ffee7344aed3d2f5fddfa1e00b3edb
Sskame1 <tolikstrgonov111@gmail.com>
2026-04-01 16:34:03 +0900

    init another. I decided to rewrite ewerything

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,49 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
 
 namespace SEWorkbenchHelper
 {
     public partial class MainWindow : Window
     {
+        private string currentFilePath = "";
+
         public MainWindow()
         {
             InitializeComponent();
 
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "C# files (*.cs)|*.cs|text files (*.txt)|*.txt|all files (*.*)|*.*";
+            
+            if (openFileDialog.ShowDialog() == true)
+            {
+                currentFilePath = openFileDialog.FileName;
+                textEditor.Load(currentFilePath);
+                this.Title = "SEWorkbenchHelper - " + currentFilePath;
+            }
+        }
+
+        private void SaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentFilePath))
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "C# files (*.cs)|*.cs|text files (*.txt)|*.txt";
+
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    currentFilePath = saveFileDialog.FileName;
+                    this.Title = "SEWorkbenchHelper - " + currentFilePath;
+                } else
+                {
+                    return;
+                }
+            }
+            textEditor.Save(currentFilePath);
         }
     }
 }
---
+++ test/testfile.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace testing {
+	class Main() {
+		int first;
+		string second = "Hello";
+	}
+}
